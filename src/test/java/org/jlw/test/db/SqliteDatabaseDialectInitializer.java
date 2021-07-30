d7cca31ff6c1606d8af5fc486033809d4047abfd
jwagner <jwagner@powercosts.com>
2021-07-30 11:29:40 -0500

    fix: corrected UUID handling (there is none)

--- src/test/java/org/jlw/test/db/SqliteDatabaseDialectInitializer.java
+++ src/test/java/org/jlw/test/db/SqliteDatabaseDialectInitializer.java
@@ -4,8 +4,6 @@ public class SqliteDatabaseDialectInitializer implements DatabaseDialectInitiali
 {
 	private static final String DEFAULT_BYTE_TABLE = "CREATE TABLE BINARY_TABLE (TEST_ID BLOB NOT NULL, VALUE_COLUMN INT NOT NULL, CONSTRAINT bin_pk PRIMARY KEY (TEST_ID))";
 
-	private static final String DEFAULT_UUID_TABLE = "CREATE TABLE UUID_TABLE (TEST_ID BLOB NOT NULL, VALUE_COLUMN INT NOT NULL, CONSTRAINT uuid_pk PRIMARY KEY (TEST_ID))";
-
 	private static final String DEFAULT_EMBEDDED_TABLE = "CREATE TABLE EMBEDDED_TABLE (TEST_ID_MSB BIGINT NOT NULL, TEST_ID_LSB BIGINT NOT NULL, VALUE_COLUMN INT NOT NULL, CONSTRAINT embedded_pk PRIMARY KEY (TEST_ID_MSB, TEST_ID_LSB))";
 
 	public String getByteBasedTable()
@@ -13,13 +11,13 @@ public class SqliteDatabaseDialectInitializer implements DatabaseDialectInitiali
 		return DEFAULT_BYTE_TABLE;
 	}
 
-	public String getUuidBasedTable()
-	{
-		return DEFAULT_UUID_TABLE;
-	}
-
 	public String getEmbeddedBasedTable()
 	{
 		return DEFAULT_EMBEDDED_TABLE;
+	}
+
+	public boolean hasUuid()
+	{
+		return false;
 	}
 }
